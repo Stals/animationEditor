abfda64006f2870f68ad3d7e8341c08391bd736f
Станислав Коротаев <superstals@gmail.com>
2012-09-01 14:02:38 +0400

    added so that you can go to next/previous frame by pressing d/a

--- animetionEditor/mainwindow.cpp
+++ animetionEditor/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow){
     ui->setupUi(this);
 
+    /* Create fileMenu */
     fileMenu = ui->menuBar->addMenu(tr("&File"));
 
     // Add save button
@@ -17,11 +18,18 @@ MainWindow::MainWindow(QWidget *parent) :
     loadAction = new QAction(tr("Load"), this);
     fileMenu->addAction(loadAction);
     connect(loadAction, SIGNAL(triggered()), this, SLOT(load()));
-
 }
+
 
 MainWindow::~MainWindow(){
     delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event){
+    switch (event->key()) {
+            case Qt::Key_A: prevFrame(); break;
+            case Qt::Key_D: nextFrame(); break;
+        }
 }
 
 void MainWindow::save(){
@@ -31,4 +39,13 @@ void MainWindow::save(){
 void MainWindow::load(){
     QString filename = QFileDialog::getOpenFileName(this,
         tr("Open Animation"), "", tr("*.ae"));
+}
+
+void MainWindow::nextFrame(){
+    ui->label->setText("next");
+}
+
+void MainWindow::prevFrame(){
+    ui->label->setText("prev");
+
 }
--- animetionEditor/mainwindow.h
+++ animetionEditor/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QFileDialog>
+#include <QKeyEvent>
 
 namespace Ui {
 class MainWindow;
@@ -14,12 +15,19 @@ class MainWindow : public QMainWindow{
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
     
 private slots:
     // Позволяет сохранить текущую анимацию в выбранный файл
     void save();
     // Позволяет загрузить анимацию из файла
     void load();
+    // Переходит на следующий кадр
+    void nextFrame();
+    // Переходит на предведущий кадр
+    void prevFrame();
 
 private:
     Ui::MainWindow *ui;
