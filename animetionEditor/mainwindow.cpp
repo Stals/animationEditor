3eb4b8c67da8d856c6c95df827e21aa5be231dc3
Станислав Коротаев <superstals@gmail.com>
2012-09-01 13:51:43 +0400

    added menu actions, and added save/load methods

--- animetionEditor/mainwindow.cpp
+++ animetionEditor/mainwindow.cpp
@@ -3,12 +3,32 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow){
     ui->setupUi(this);
+
+    fileMenu = ui->menuBar->addMenu(tr("&File"));
+
+    // Add save button
+    saveAction = new QAction(tr("Save as..."), this);
+    fileMenu->addAction(saveAction);
+    connect(saveAction, SIGNAL(triggered()), this, SLOT(save()));
+
+    // Add load button
+    loadAction = new QAction(tr("Load"), this);
+    fileMenu->addAction(loadAction);
+    connect(loadAction, SIGNAL(triggered()), this, SLOT(load()));
+
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
+}
+
+void MainWindow::save(){
+
+}
+
+void MainWindow::load(){
+    QString filename = QFileDialog::getOpenFileName(this,
+        tr("Open Animation"), "", tr("*.ae"));
 }
--- animetionEditor/mainwindow.h
+++ animetionEditor/mainwindow.h
@@ -2,21 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow{
     Q_OBJECT
     
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    // Позволяет сохранить текущую анимацию в выбранный файл
+    void save();
+    // Позволяет загрузить анимацию из файла
+    void load();
+
 private:
     Ui::MainWindow *ui;
+    QMenu *fileMenu;
+    QAction *saveAction;
+    QAction *loadAction;
+
 };
 
 #endif // MAINWINDOW_H
